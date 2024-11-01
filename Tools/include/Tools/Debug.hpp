feb00d08d0aef50020ece46ecfd6fef777b6ce93
gpinchon <pinchon.guillaume@gmail.com>
2024-11-01 15:29:59 +0100

    improve debug log

--- Tools/include/Tools/Debug.hpp
+++ Tools/include/Tools/Debug.hpp
@@ -11,15 +11,15 @@
 
 #define _consoleStream(func, line) std::cout << "Message : " << func << " at line [" << line << "] : "
 #define _errorStream(func, line)   std::cerr << "Error   : " << func << " at line [" << line << "] : "
+#define _debugStream(func, line)   std::cerr << "Debug   : " << __DATE__ << " " << __TIME__ << " | " << func << " at line [" << line << "] : "
 
+#define debugStream \
+    if (NDEBUG)     \
+        ;           \
+    else            \
+        _debugStream(__FUNCTION__, __LINE__)
 #define consoleStream       _consoleStream(__FUNCTION__, __LINE__)
 #define errorStream         _errorStream(__FUNCTION__, __LINE__)
 #define consoleLog(message) consoleStream << (message) << std::endl;
 #define errorLog(message)   errorStream << (message) << std::endl;
-
-#ifndef NDEBUG
-#define _debugStream(func, line) std::cerr << "Debug   : " << __DATE__ << " " << __TIME__ << " | " << func << " at line [" << line << "] : "
-#define debugLog(message)        _debugStream(__FUNCTION__, __LINE__) << message << std::endl;
-#else
-#define debugLog(message) void(message);
-#endif
+#define debugLog(message)   debugStream << (message) << std::endl;
