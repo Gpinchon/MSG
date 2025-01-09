f971c307db8462c4a7edeb0253c28069ef361cdb
gpinchon <pinchon.guillaume@gmail.com>
2025-01-09 22:29:07 +0100

    Window WIP

--- Window/include/Window/Window.hpp
+++ Window/include/Window/Window.hpp
@@ -1,6 +1,22 @@
+#include <Renderer/Handles.hpp>
 #include <Window/Handles.hpp>
 #include <Window/Structs.hpp>
 
+#include <any>
+
 namespace TabGraph::Window {
-Handle Create(const CreateWindowInfo& a_Info);
+Handle Create(const Renderer::Handle& a_Renderer, const CreateWindowInfo& a_Info);
+void Show(const Handle& a_Window);
+void Present(const Handle& a_Window, const Renderer::RenderBuffer::Handle& a_RenderBuffer);
+bool IsClosing(const Handle& a_Window);
+uint32_t GetWidth(const Handle& a_Window);
+uint32_t GetHeight(const Handle& a_Window);
+Renderer::SwapChain::Handle GetSwapChain(const Handle& a_Window);
+#ifdef _WIN32
+std::any GetNativeWindowHandle(const Handle& a_Window);
+std::any GetNativeDisplayHandle(const Handle& a_Window);
+#elif defined __linux
+std::any GetNativeWindowHandle(const Handle& a_Window);
+std::any GetNativeDisplayHandle(const Handle& a_Window);
+#endif
 }
