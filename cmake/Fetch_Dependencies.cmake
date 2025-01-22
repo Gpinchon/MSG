7b1af0fb9102001b3b2123449496bb81c2fd74f1
gpinchon <pinchon.guillaume@gmail.com>
2025-01-23 00:17:25 +0100

    mofe Fetch_GLEW to top level + various fixes

--- cmake/Fetch_Dependencies.cmake
+++ cmake/Fetch_Dependencies.cmake
@@ -1,5 +1,6 @@
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Fetch_GoogleTest.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Fetch_GLM.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Fetch_GLEW.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Fetch_ZLib.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Fetch_FasTC.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Fetch_SparseHash.cmake)
