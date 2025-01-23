5810c55db90f88119292ccd52b137a09940c110d
gpinchon <pinchon.guillaume@gmail.com>
2025-01-23 15:12:18 +0100

    fix glew dependency

--- cmake/Fetch_GLEW.cmake
+++ cmake/Fetch_GLEW.cmake
@@ -2,6 +2,7 @@ include(FetchContent)
 
 # Fetch GLEW
 macro(Fetch_GLEW)
+  option(GLEW_USE_STATIC_LIBS "" TRUE)
   find_package(GLEW 2.2.0 QUIET)
   if (NOT GLEW_FOUND)
     if (WIN32)
@@ -10,17 +11,27 @@ macro(Fetch_GLEW)
         GIT_REPOSITORY  https://github.com/Perlmint/glew-cmake.git
         GIT_TAG         glew-cmake-2.2.0
       )
-      option(ONLY_LIBS "Do not build executables" ON)
-      option(glew-cmake_BUILD_STATIC "Build the static glew library" ON)
-      option(glew-cmake_BUILD_SHARED "Build the shared glew library" OFF)
-      FetchContent_MakeAvailable(GLEW)
-      add_library(GLEW::glew_s ALIAS libglew_static)
-      add_library(GLEW::GLEW ALIAS libglew_static)
-      set(GLEW_FOUND CACHE INTERNAL BOOL "GLEW found" ON)
-      set(GLEW_INCLUDE_DIRS ${glew_SOURCE_DIR}/include)
-      set(GLEW_STATIC_LIBRARIES GLEW::glew_s)
-      set(GLEW_SHARED_LIBRARIES GLEW::glew)
-      set_subdirectory_folder("3rdparty/GLEW" ${glew_SOURCE_DIR})
+      FetchContent_GetProperties(GLEW)
+      if (NOT glew_POPULATED)
+        FetchContent_Populate(GLEW)
+      endif(NOT glew_POPULATED)
+      execute_process(
+        COMMAND ${CMAKE_COMMAND}
+          "-DONLY_LIBS=ON"
+          "-Dglew-cmake_BUILD_STATIC=ON"
+          "-Dglew-cmake_BUILD_SHARED=OFF"
+          "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+          -G ${CMAKE_GENERATOR}
+          -S ${glew_SOURCE_DIR}
+          -B ${glew_BINARY_DIR}
+          --install-prefix ${CMAKE_BINARY_DIR}/external)
+      execute_process(
+        COMMAND ${CMAKE_COMMAND}
+          --build ${glew_BINARY_DIR})
+      execute_process(
+        COMMAND ${CMAKE_COMMAND}
+          --install ${glew_BINARY_DIR})
+      
     elseif (LINUX)
       FetchContent_Declare(
         GLEW
@@ -30,7 +41,7 @@ macro(Fetch_GLEW)
       FetchContent_GetProperties(GLEW)
       if (NOT glew_POPULATED)
         FetchContent_Populate(GLEW)
-        set(GLEW_DEST "GLEW_DEST=${glew_BINARY_DIR}")
+        set(GLEW_DEST "GLEW_DEST=${CMAKE_BINARY_DIR}/external")
         set(GLEW_BUILD_VARS "GLEW_NO_GLU=-DGLEW_NO_GLU CC=${CMAKE_CXX_COMPILER} SYSTEM=linux-egl")
         execute_process(
           COMMAND make extensions ${GLEW_BUILD_VARS} ${GLEW_DEST}
@@ -46,13 +57,14 @@ macro(Fetch_GLEW)
           WORKING_DIRECTORY ${glew_SOURCE_DIR})
         message("GLEW fetched to ${glew_SOURCE_DIR}")
       endif (NOT glew_POPULATED)
-
       list(APPEND CMAKE_PREFIX_PATH ${glew_BINARY_DIR})
-      find_package(GLEW 2.2.0 REQUIRED)
-      
     endif (WIN32)
+    find_package(GLEW 2.2.0 REQUIRED)
   ENDIF (NOT GLEW_FOUND)
+  find_package(OpenGL REQUIRED)
+  set(OPENGL_LIBRARIES GLEW::GLEW OpenGL::GL)
   message("GLEW_INCLUDE_DIRS     : ${GLEW_INCLUDE_DIRS}")
   message("GLEW_STATIC_LIBRARIES : ${GLEW_STATIC_LIBRARIES}")
   message("GLEW_SHARED_LIBRARIES : ${GLEW_SHARED_LIBRARIES}")
+  message("OPENGL_LIBRARIES      : ${OPENGL_LIBRARIES}")
 endmacro()
