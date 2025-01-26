34d5c841219e0fb0bde32fc7b18b7558ad8ea1fe
gpinchon <pinchon.guillaume@gmail.com>
2025-01-26 21:14:00 +0100

    fix shared context

--- libs/OGLContext/Driver/Win32/include/MSG/OGLContext/Win32/WGL.hpp
+++ libs/OGLContext/Driver/Win32/include/MSG/OGLContext/Win32/WGL.hpp
@@ -10,14 +10,14 @@ class HDCWrapper;
 namespace WGL {
 class HGLRCWrapper {
 public:
-    HGLRCWrapper(const std::shared_ptr<Win32::HDCWrapper>& a_HDCWrapper, const std::any& a_SharedHGLRC, const bool& a_SetPixelFormat);
+    HGLRCWrapper(const std::shared_ptr<Win32::HDCWrapper>& a_HDCWrapper, const HGLRCWrapper* a_SharedHGLRC, const bool& a_SetPixelFormat);
     ~HGLRCWrapper();
     std::shared_ptr<Win32::HDCWrapper> hdcWrapper;
     std::any hglrc;
 };
 
 void InitOGL();
-std::any CreateContext(const std::any& a_HDC, const std::any& a_SharedHGLRC);
+std::any CreateContext(const std::any& a_HDC, const HGLRCWrapper* a_SharedHGLRC);
 int32_t GetDefaultPixelFormat(const std::any& a_HDC);
 uint64_t GetID(const std::any& a_HGLRC);
 void MakeCurrent(const std::any& a_HDC, const std::any& a_HGLRC);
