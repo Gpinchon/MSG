02803e64ea579f0f9cc5f12157f671474ea81742
gpinchon <pinchon.guillaume@gmail.com>
2025-02-02 14:24:42 +0100

    fix debug compilation

--- libs/OGL/OGLDebugGroup/src/MSG/OGLDebugGroup.cpp
+++ libs/OGL/OGLDebugGroup/src/MSG/OGLDebugGroup.cpp
@@ -3,14 +3,13 @@
 #include <GL/glew.h>
 
 namespace MSG {
-
 OGLDebugGroup::OGLDebugGroup(const std::string& a_Name)
 {
 #ifndef NDEBUG
     glPushDebugGroup(
         GL_DEBUG_SOURCE_APPLICATION,
-        std::hash<std::string> {}(name),
-        name.size(), name.c_str());
+        std::hash<std::string> {}(a_Name),
+        a_Name.size(), a_Name.c_str());
 #endif
 }
 
