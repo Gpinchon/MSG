cf5267ec6c0c9ac1f65b2b05b3da1154d46cb2ac
gpinchon <pinchon.guillaume@gmail.com>
2025-02-18 16:40:05 +0100

    seamless cubemaps WIP

--- libs/Texture/include/MSG/Sampler.hpp
+++ libs/Texture/include/MSG/Sampler.hpp
@@ -16,6 +16,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 namespace MSG {
 class Texture;
+class Image;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -76,6 +77,8 @@ public:
     PROPERTY(glm::vec4, BorderColor, 0.f, 0.f, 0.f, 1.f);
 
 public:
+    MSG::SamplerFilter GetImageFilter() const;
+    MSG::SamplerFilter GetMipmapFilter() const;
     auto& GetWrapS() const { return GetWrapModes().at(0); }
     auto& GetWrapT() const { return GetWrapModes().at(1); }
     auto& GetWrapR() const { return GetWrapModes().at(2); }
@@ -86,59 +89,71 @@ public:
 
 class Sampler1D : public Sampler {
 public:
+    using Sampler::Sampler;
     Sampler1D(const Sampler& a_Other)
         : Sampler(a_Other)
     {
     }
+    glm::vec4 Sample(const Image& a_Image, const glm::vec1& a_UV) const;
     glm::vec4 Sample(const Texture& a_Texture, const glm::vec1& a_UV, const float& a_Lod = 0) const;
     glm::vec4 TexelFetch(const Texture& a_Texture, const glm::ivec1& a_TexelCoord, const uint32_t& a_Lod) const;
 };
 
 class Sampler2D : public Sampler {
 public:
+    using Sampler::Sampler;
     Sampler2D(const Sampler& a_Other)
         : Sampler(a_Other)
     {
     }
+    glm::vec4 Sample(const Image& a_Image, const glm::vec2& a_UV) const;
     glm::vec4 Sample(const Texture& a_Texture, const glm::vec2& a_UV, const float& a_Lod = 0) const;
     glm::vec4 TexelFetch(const Texture& a_Texture, const glm::ivec2& a_TexelCoord, const uint32_t& a_Lod) const;
 };
 
 class Sampler3D : public Sampler {
 public:
+    using Sampler::Sampler;
     Sampler3D(const Sampler& a_Other)
         : Sampler(a_Other)
     {
     }
+    glm::vec4 Sample(const Image& a_Image, const glm::vec3& a_UV) const;
     glm::vec4 Sample(const Texture& a_Texture, const glm::vec3& a_UV, const float& a_Lod = 0) const;
     glm::vec4 TexelFetch(const Texture& a_Texture, const glm::ivec3& a_TexelCoord, const uint32_t& a_Lod) const;
 };
 
 class SamplerCube : public Sampler {
 public:
+    using Sampler::Sampler;
     SamplerCube(const Sampler& a_Other)
         : Sampler(a_Other)
     {
     }
-    glm::vec4 Sample(const Texture& a_Texture, const glm::vec3& a_UV, const float& a_Lod = 0) const;
+    glm::vec4 Sample(const Image& a_Image, const glm::vec3& a_Dir) const;
+    glm::vec4 Sample(const Texture& a_Texture, const glm::vec3& a_Dir, const float& a_Lod = 0) const;
 };
 
 class Sampler1DArray : public Sampler {
 public:
+    using Sampler::Sampler;
     Sampler1DArray(const Sampler& a_Other)
         : Sampler(a_Other)
     {
     }
+    glm::vec4 Sample(const Image& a_Image, const glm::vec2& a_UV) const;
     glm::vec4 Sample(const Texture& a_Texture, const glm::vec2& a_UV, const float& a_Lod = 0) const;
     glm::vec4 TexelFetch(const Texture& a_Texture, const glm::ivec2& a_TexelCoord, const uint32_t& a_Lod) const;
 };
 
 class Sampler2DArray : public Sampler {
 public:
+    using Sampler::Sampler;
     Sampler2DArray(const Sampler& a_Other)
         : Sampler(a_Other)
     {
     }
+    glm::vec4 Sample(const Image& a_Image, const glm::vec3& a_UV) const;
     glm::vec4 Sample(const Texture& a_Texture, const glm::vec3& a_UV, const float& a_Lod = 0) const;
     glm::vec4 TexelFetch(const Texture& a_Texture, const glm::ivec3& a_TexelCoord, const uint32_t& a_Lod) const;
 };
