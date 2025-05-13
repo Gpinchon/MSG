17df9957d9815256ee42517d76eabcf8d4586381
gpinchon <pinchon.guillaume@gmail.com>
2025-05-13 11:52:50 +0200

    cleanup GaussianBlur

--- libs/Renderer/Driver/OpenGL/Shaders/header/GaussianBlur.glsl
+++ libs/Renderer/Driver/OpenGL/Shaders/header/GaussianBlur.glsl
@@ -8,12 +8,29 @@
 #ifdef __cplusplus
 namespace MSG::Renderer::GLSL {
 #endif //__cplusplus
-
 struct GaussianBlurSettings {
     vec2 direction;
     vec2 scale;
 };
+#ifndef __cplusplus
+#include <Bicubic.glsl>
 
+const float GaussianBlurWeights[5] = float[5](0.16, 0.15, 0.12, 0.09, 0.05);
+
+vec4 GaussianBlur(IN(sampler2D) a_Sampler, IN(vec2) a_UV, IN(GaussianBlurSettings) a_Settings)
+{
+    const vec2 texelSize = vec2(1.f / textureSize(a_Sampler, 0).xy);
+    const vec2 offset    = (texelSize * a_Settings.direction * a_Settings.scale) / float(GaussianBlurWeights.length());
+    vec4 outColor        = textureNice(a_Sampler, a_UV) * GaussianBlurWeights[0];
+    for (uint i = 1; i < GaussianBlurWeights.length(); ++i) {
+        const vec2 UV0 = a_UV + (offset * float(i));
+        const vec2 UV1 = a_UV - (offset * float(i));
+        outColor += textureNice(a_Sampler, UV0) * GaussianBlurWeights[i];
+        outColor += textureNice(a_Sampler, UV1) * GaussianBlurWeights[i];
+    }
+    return outColor;
+}
+#endif
 #ifdef __cplusplus
 struct GaussianBlurSettingsUBO : GaussianBlurSettings {
     uint _padding[60];
--- libs/Renderer/Driver/OpenGL/Shaders/stage/GaussianBlur.frag
+++ libs/Renderer/Driver/OpenGL/Shaders/stage/GaussianBlur.frag
@@ -1,5 +1,4 @@
 #include <GaussianBlur.glsl>
-#include <Bicubic.glsl>
 
 layout(binding = 0) uniform sampler2D u_Color;
 
@@ -13,17 +12,7 @@ layout(binding = 0) uniform SettingsBlock
     GaussianBlurSettings u_Settings;
 };
 
-const float weight[5] = float[5](0.16, 0.15, 0.12, 0.09, 0.05);
-
 void main()
 {
-    const vec2 offset = (vec2(1.f / textureSize(u_Color, 0).xy) * u_Settings.direction * u_Settings.scale) / float(weight.length());
-	vec4 outColor     = textureNice(u_Color, in_UV) * weight[0];
-    for (uint i = 1; i < weight.length(); ++i) {
-        const vec2 UV0 = in_UV + (offset * float(i));
-        const vec2 UV1 = in_UV - (offset * float(i));
-        outColor += textureNice(u_Color, UV0) * weight[i];
-        outColor += textureNice(u_Color, UV1) * weight[i];
-    }
-	out_Color0 = out_Color1 = outColor;
+    out_Color0 = out_Color1 = GaussianBlur(u_Color, in_UV, u_Settings);
 }
