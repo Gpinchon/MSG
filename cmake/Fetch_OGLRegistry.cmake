d46c7ea95eb809a036631f5cca20cc5def61d376
gpinchon <pinchon.guillaume@gmail.com>
2025-08-23 16:37:32 +0200

    specify precise git tag for OpenGL-Registry to avoid future breakage

--- cmake/Fetch_OGLRegistry.cmake
+++ cmake/Fetch_OGLRegistry.cmake
@@ -1,8 +1,8 @@
 macro(Fetch_OGLRegistry)
   FetchContent_Declare(
       GL_REGISTRY
-      GIT_TAG        main
       GIT_REPOSITORY https://github.com/KhronosGroup/OpenGL-Registry/
+      GIT_TAG        98c707a87ef63aae2ac8355abf59a6fb1823a6f3
   )
   FetchContent_MakeAvailable(GL_REGISTRY)
   set(GL_INCLUDE_DIRS
