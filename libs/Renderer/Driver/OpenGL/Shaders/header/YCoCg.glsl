7639e3c265a8b2ce7e64ff309046718ab58763eb
gpinchon <pinchon.guillaume@gmail.com>
2025-10-17 17:49:50 +0200

    rename YCoCg variables for coherency

--- libs/Renderer/Driver/OpenGL/Shaders/header/YCoCg.glsl
+++ libs/Renderer/Driver/OpenGL/Shaders/header/YCoCg.glsl
@@ -5,22 +5,22 @@
 #include <Types.glsl>
 namespace Msg::Renderer::GLSL {
 #endif
-vec4 RGBA2YCoCgA(vec4 inRGBA)
+vec4 RGBA2YCoCgA(vec4 a_RGBA)
 {
     const mat3 convMat = mat3(
         1 / 4.f, 1 / 2.f, 1 / 4.f,
         1 / 2.f, 0.f, -1 / 2.f,
         -1 / 4.f, 1 / 2.f, -1 / 4.f);
-    return vec4(convMat * inRGBA.rgb, inRGBA.a);
+    return vec4(convMat * a_RGBA.rgb, a_RGBA.a);
 }
 
-vec4 YCoCgA2RGBA(vec4 inYCoCgA)
+vec4 YCoCgA2RGBA(vec4 a_YCoCgA)
 {
     const mat3 convMat = mat3(
         1, 1, -1,
         1, 0, 1,
         1, -1, -1);
-    return vec4(convMat * inYCoCgA.rgb, inYCoCgA.a);
+    return vec4(convMat * a_YCoCgA.rgb, a_YCoCgA.a);
 }
 #endif __cplusplus
 
