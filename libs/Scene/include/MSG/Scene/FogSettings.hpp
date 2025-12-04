22dc9ec413419c64785cdd097a457c1d4e153f38
gpinchon <pinchon.guillaume@gmail.com>
2025-12-04 18:12:41 +0100

    update FogSettings doc

--- libs/Scene/include/MSG/Scene/FogSettings.hpp
+++ libs/Scene/include/MSG/Scene/FogSettings.hpp
@@ -11,9 +11,9 @@ struct VolumetricFogSettings {
     float minDistance            = 0.5f; // the distance at which the volumetric fog should start rendering
     float maxDistance            = 1000.f; // the max distance the volumetric fog will be displayed
     float depthExp               = 5.f; // the depth exponent used to render volumetric fog
-    glm::vec3 noiseDensityOffset = { 0, 0, 0 };
-    float noiseDensityScale      = 0.5f;
-    float noiseDensityIntensity  = 1.0f;
+    glm::vec3 noiseDensityOffset = { 0, 0, 0 }; // the offset inside the noise texture
+    float noiseDensityScale      = 0.5f; // the scale of the sampling inside the noise texture
+    float noiseDensityIntensity  = 1.0f; // the intensity of the noise in the final render
     float noiseDensityMaxDist    = 100.f; // the maximum depth at which density noise will be applied before being progressively reduced
 };
 struct FogSettings {
