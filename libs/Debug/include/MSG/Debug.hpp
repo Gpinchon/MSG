b21a11cb9336269bdb7353deb3358e18f93c0454
gpinchon <pinchon.guillaume@gmail.com>
2026-02-23 23:38:14 +0100

    fix MsgDebugStream throwing an error

--- libs/Debug/include/MSG/Debug.hpp
+++ libs/Debug/include/MSG/Debug.hpp
@@ -4,7 +4,7 @@
 
 #define _MSGConsoleStream(func, line) std::cout << "Message : " << func << " at line [" << line << "] : "
 #define _MSGErrorStream(func, line)   std::cerr << "Error   : " << func << " at line [" << line << "] : "
-#define _MSGDebugStream(func, line)   std::cerr << "Debug   : " << __DATE__ << " " << __TIME__ << " | " << func << " at line [" << line << "] : "
+#define _MSGDebugStream(func, line)   std::cout << "Debug   : " << __DATE__ << " " << __TIME__ << " | " << func << " at line [" << line << "] : "
 
 #ifdef MSG_DEBUG
 #define MSGDebugStream _MSGDebugStream(__FUNCTION__, __LINE__)
